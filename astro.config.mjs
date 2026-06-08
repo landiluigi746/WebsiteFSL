063c3b2613de4d06d5f2dfd9e82d92681d504faa
landiluigi746 <landiluigi865@gmail.com>
2026-06-09 01:30:44 +0200

    [feat]: Reaplce global font with Google Sans

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: "Google Sans",
+      cssVariable: "--font-google-sans",
+    },
+  ],
 });
