e6c92ae9563ec2f2c6de46e066d9b6dc29173047
landiluigi746 <landiluigi865@gmail.com>
2026-06-10 00:14:07 +0200

    [wip]: Scaffold projects section in homepage with sample data

---
+++ src/data/projects.ts
@@ -0,0 +1,38 @@
+export type Project = {
+  title: string;
+  years: Array<string>;
+  description: string;
+};
+
+export const projects: Project[] = [
+  {
+    title: "CyberChallenge.IT",
+    years: ["2024-2025"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum justo dui, eget malesuada augue luctus id. Aliquam eleifend faucibus ex ut tincidunt.",
+  },
+  {
+    title: "OliCyber.IT",
+    years: ["2024-2025", "2025-2026"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum justo dui, eget malesuada augue luctus id. Aliquam eleifend faucibus ex ut tincidunt.",
+  },
+  {
+    title: "Phaser Game Jam",
+    years: ["2023-2024", "2024-2025", "2025-2026"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum justo dui, eget malesuada augue luctus id. Aliquam eleifend faucibus ex ut tincidunt.",
+  },
+  {
+    title: "Siviglia",
+    years: ["2024-2025"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum justo dui, eget malesuada augue luctus id. Aliquam eleifend faucibus ex ut tincidunt.",
+  },
+  {
+    title: "Eipass",
+    years: ["2023-2024"],
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut condimentum justo dui, eget malesuada augue luctus id. Aliquam eleifend faucibus ex ut tincidunt.",
+  },
+];
